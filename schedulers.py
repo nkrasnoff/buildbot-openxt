5d56dd0214d3959db3db0e8575f20f91fa4bc046
Eric Chanudet <chanudete@ainfosec.com>
2020-09-14 18:40:05 -0400

    nightly: Trigger nightly builds of various version
    Add nightly builds of:
    - stable-9 tips (9-custom-clean-legacy and 9-custom-quick-legacy)
    - master tips (master-custom-clean-legacy and master-custom-quick-legacy)
    - windows tools (wintools)
    
    Try to stage the builds in time a little bit.
    
    Signed-off-by: Eric Chanudet <chanudete@ainfosec.com>

--- schedulers.py
+++ schedulers.py
@@ -58,16 +58,16 @@ def scheduler_force_stable(name, builders, template_dfl, codebases_stable):
             )
         ])
 
-def scheduler_nightly_stable(name, builders, template_dfl, codebases_stable):
+def scheduler_nightly(name, builders, template_dfl, codebases, hour, minute):
     return schedulers.Nightly(
         name=name,
-        codebases=codebases_stable,
+        codebases=codebases,
         properties={
             'template': template_dfl
         },
         builderNames=builders,
-        hour=22,
-        minute=0,
+        hour=hour,
+        minute=minute,
         onlyIfChanged=True)
 
 def scheduler_force_wintools(name, builders, codebases_wintools):
@@ -81,3 +81,12 @@ def scheduler_force_wintools(name, builders, codebases_wintools):
         builderNames=builders,
         codebases=codebases_to_params(codebases_wintools)
     )
+
+def scheduler_nightly_wintools(name, builders, codebases, hour, minute):
+    return schedulers.Nightly(
+        name=name,
+        codebases=codebases,
+        builderNames=builders,
+        hour=hour,
+        minute=minute,
+        onlyIfChanged=True)
