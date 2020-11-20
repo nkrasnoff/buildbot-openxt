0891c1e4403c0f8b82748b1aa1d204b4f12b3e0a
Eric Chanudet <chanudete@ainfosec.com>
2020-11-20 11:22:58 -0500

    zeus: set default sub-project revisions
    Zeus sub-projects are on fixed revisions. Custom builds most likely will
    change only a few of these, so provide the ones tracked in the layer as
    default for the others.
    
    Signed-off-by: Eric Chanudet <chanudete@ainfosec.com>

--- config_custom_zeus.py
+++ config_custom_zeus.py
@@ -58,7 +58,7 @@ codebases_custom_zeus = {
     'bats-suite': {
         'repository': 'git://github.com/OpenXT/bats-suite.git',
         'branch': 'master',
-        'revision': '',
+        'revision': '4615057adc2670bc558d9322fa7941363eda3b9c',
     },
     'bordel': {
         'repository': 'git://github.com/eric-ch/bordel.git',
@@ -68,116 +68,111 @@ codebases_custom_zeus = {
     'fbtap': {
         'repository': 'git://github.com/OpenXT/fbtap.git',
         'branch': 'master',
-        'revision': '',
+        'revision': '30fd6ec306b188030a2fa58cde29c3e7f129c908',
     },
     'icbinn': {
         'repository': 'git://github.com/OpenXT/icbinn.git',
         'branch': 'master',
-        'revision': '',
+        'revision': 'dacb22e8499c99774375d0b7bad0f2880c734204',
     },
     'idl': {
         'repository': 'git://github.com/OpenXT/idl.git',
         'branch': 'master',
-        'revision': '',
+        'revision': '7ea58aa0afd1d3059a6c9ebe4cd304bad669204c',
     },
     'input': {
         'repository': 'git://github.com/OpenXT/input.git',
         'branch': 'master',
-        'revision': '',
+        'revision': '799c4861b59b100a5fbd68d406821125de714d9d',
     },
     'installer': {
         'repository': 'git://github.com/OpenXT/installer.git',
         'branch': 'master',
-        'revision': '',
+        'revision': '8831fe9ab2f57c72b8c99ff9ebbddf0e2254c6e5',
     },
     'libedid': {
         'repository': 'git://github.com/OpenXT/libedid.git',
         'branch': 'master',
-        'revision': '',
+        'revision': 'bad8ca87e4649e9aa931c607e6a17a7881c2cfd6',
     },
     'libxcdbus': {
         'repository': 'git://github.com/OpenXT/libxcdbus.git',
         'branch': 'master',
-        'revision': '',
+        'revision': '53650007eeb04abcccc72ae862632203cbbd6214',
     },
     'libxenbackend': {
         'repository': 'git://github.com/OpenXT/libxenbackend.git',
         'branch': 'master',
-        'revision': '',
+        'revision': '66c02bd96475a63f7ae3ed3f1300c8f9dcd5031e',
     },
     'linux-xen-argo': {
         'repository': 'git://github.com/OpenXT/linux-xen-argo.git',
         'branch': 'master',
-        'revision': '',
+        'revision': 'd84cf67969c5510e7e8f18dba2da62c68b565a2e',
     },
     'manager': {
         'repository': 'git://github.com/OpenXT/manager.git',
         'branch': 'master',
-        'revision': '',
+        'revision': 'b28ccc4d241728f6b840779328820cd1387bd14e',
     },
     'network': {
         'repository': 'git://github.com/OpenXT/network.git',
         'branch': 'master',
-        'revision': '',
-    },
-    'pv-linux-drivers': {
-        'repository': 'git://github.com/OpenXT/pv-linux-drivers.git',
-        'branch': 'master',
-        'revision': '',
+        'revision': 'dbf350a0c0f18c8f9dd41c8cda9473fb700c2aa9',
     },
     'surfman': {
         'repository': 'git://github.com/OpenXT/surfman.git',
         'branch': 'master',
-        'revision': '',
+        'revision': '46bfde5850d92399d36edb24313f2e404d53d4dd',
     },
     'sync-client': {
         'repository': 'git://github.com/OpenXT/sync-client.git',
         'branch': 'master',
-        'revision': '',
+        'revision': 'd501139ec156b8f3de5f7150f5d3559de0362fa9',
     },
     'sync-wui': {
         'repository': 'git://github.com/OpenXT/sync-wui.git',
         'branch': 'master',
-        'revision': '',
+        'revision': 'e37a0993f7821365e7dae1e969a837125df418de',
     },
     'toolstack-data': {
         'repository': 'git://github.com/OpenXT/toolstack-data.git',
         'branch': 'master',
-        'revision': '',
+        'revision': 'cb6dd61aee762507dbd432f43b012068353ed0d0',
     },
     'toolstack': {
         'repository': 'git://github.com/OpenXT/toolstack.git',
         'branch': 'master',
-        'revision': '',
+        'revision': '8e1f91f340a38203c9d0d95e672e3a94c2137aaf',
     },
     'uid': {
         'repository': 'git://github.com/OpenXT/uid.git',
         'branch': 'master',
-        'revision': '',
+        'revision': '30dc9152274b92ca4a73813b43477deac3c24442',
     },
     'vusb-daemon': {
         'repository': 'git://github.com/OpenXT/vusb-daemon.git',
         'branch': 'master',
-        'revision': '',
+        'revision': '9877a53cf64d1f7a03b2637b59fff281ce721e4c',
     },
     'xclibs': {
         'repository': 'git://github.com/OpenXT/xclibs.git',
         'branch': 'master',
-        'revision': '',
+        'revision': 'ef260585087aa24fff09d5d8d75c7e9cd70c0f30',
     },
     'xctools': {
         'repository': 'git://github.com/OpenXT/xctools.git',
         'branch': 'master',
-        'revision': '',
+        'revision': '8abe7f9aa43cd7a95b958e28e80fbfa2bea1c682',
     },
     'xenfb2': {
         'repository': 'git://github.com/OpenXT/xenfb2.git',
         'branch': 'master',
-        'revision': '',
+        'revision': '33dd2ddd9e083bae500dc6710ffb4f915a8a8ef8',
     },
     'xsm-policy': {
         'repository': 'git://github.com/OpenXT/xsm-policy.git',
         'branch': 'master',
-        'revision': '',
+        'revision': '872126eb809f8be90bd5eb9850d6fe673ce3025c',
     }
 }
