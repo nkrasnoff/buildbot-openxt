aec822a42dfc80d11db7597d98b5aa2c50cc8afe
Eric Chanudet <chanudete@ainfosec.com>
2020-12-07 14:38:03 -0500

    wintools: track upstream branches
    pull-requests required for the buildbot worker to provide build
    artefacts were merged some time ago.
    
    Signed-off-by: Eric Chanudet <chanudete@ainfosec.com>

--- config_wintools.py
+++ config_wintools.py
@@ -1,18 +1,18 @@
 # Source configuration for OpenXT Windows tools. 
 codebases_wintools = {
     'openxt': {
-        'repository': 'git://github.com/eric-ch/openxt.git',
-        'branch': 'build-fixes-1',
+        'repository': 'git://github.com/OpenXT/openxt.git',
+        'branch': 'master',
         'revision': '',
     },
     'xc-windows': {
-        'repository': 'git://github.com/eric-ch/xc-windows.git',
-        'branch': 'build-fixes-0',
+        'repository': 'git://github.com/OpenXT/xc-windows.git',
+        'branch': 'master',
         'revision': '',
     },
     'msi-installer': {
-        'repository': 'git://github.com/eric-ch/msi-installer.git',
-        'branch': 'build-fixes-0',
+        'repository': 'git://github.com/OpenXT/msi-installer.git',
+        'branch': 'master',
         'revision': '',
     },
     'xc-vusb': {
